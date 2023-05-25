0ed2671cafd8d9f579d54c9bd2d9ccc4ed488b71
MartinOpat <martinopat.opat@gmail.com>
2023-05-25 21:25:29 +0200

    Added manual repathing of coverage report

--- document_processor/generate_unit_tests_report.sh
+++ document_processor/generate_unit_tests_report.sh
@@ -1,1 +1,11 @@
 pytest --cov --cov-config=.coveragerc --cov-report xml:reports/coverage.xml --junitxml=reports/pytest.xml
+
+# The paths to replace
+old_path="src/"
+new_path="document_processor\/src\/"
+
+# The coverage report file
+file="reports/coverage.xml"
+
+# Use sed to perform the replacement
+sed -i "s#${old_path}#${new_path}#g" "${file}"
