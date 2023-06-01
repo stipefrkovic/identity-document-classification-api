4011042635c3aa9b9eb9740bc6a35b9eda42d261
MartinOpat <martinopat.opat@gmail.com>
2023-06-01 15:09:20 +0200

    Refactored test_pdf_image_converter.py

--- document_processor/src/tests/pipeline/test_pdf_to_image_converter.py
+++ document_processor/src/tests/pipeline/test_pdf_to_image_converter.py
@@ -24,9 +24,16 @@ class TestPdfToJpgConverter:
     @pytest.fixture(params=[1, 2])
     def pdf_and_image_bytes(self, request):
         if request.param == 1:
-            pdf_bytes = b"%PDF-1.7\n1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj\n2 0 obj<</Type/Pages/Count 1/Kids[3 0 R]>>endobj\n3 0 obj<</Type/Page/MediaBox[0 0 595 842]>>endobj\nxref\n0 4\n0000000000 65535 f\n0000000018 00000 n\n0000000063 00000 n\n0000000108 00000 n\ntrailer<</Size 4/Root 1 0 R>>startxref\n142\n%%EOF\n"
+            pdf_bytes = b"%PDF-1.7\n1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj\n2 0 obj<</Type/Pages/Count 1/Kids[3 0 " \
+                        b"R]>>endobj\n3 0 obj<</Type/Page/MediaBox[0 0 595 842]>>endobj\nxref\n0 4\n0000000000 65535 " \
+                        b"f\n0000000018 00000 n\n0000000063 00000 n\n0000000108 00000 n\ntrailer<</Size 4/Root 1 0 " \
+                        b"R>>startxref\n142\n%%EOF\n"
         else:
-            pdf_bytes = b"%PDF-1.7\n1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj\n2 0 obj<</Type/Pages/Kids[3 0 R 4 0 R]/Count 2>>endobj\n3 0 obj<</Type/Page/MediaBox[0 0 595 842]>>endobj\n4 0 obj<</Type/Page/MediaBox[0 0 595 842]>>endobj\nxref\n0 5\n0000000000 65535 f\n0000000018 00000 n\n0000000062 00000 n\n0000000106 00000 n\n0000000150 00000 n\ntrailer<</Size 5/Root 1 0 R>>startxref\n146\n%%EOF\n"
+            pdf_bytes = b"%PDF-1.7\n1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj\n2 0 obj<</Type/Pages/Kids[3 0 R 4 0 " \
+                        b"R]/Count 2>>endobj\n3 0 obj<</Type/Page/MediaBox[0 0 595 842]>>endobj\n4 0 " \
+                        b"obj<</Type/Page/MediaBox[0 0 595 842]>>endobj\nxref\n0 5\n0000000000 65535 f\n0000000018 " \
+                        b"00000 n\n0000000062 00000 n\n0000000106 00000 n\n0000000150 00000 n\ntrailer<</Size 5/Root " \
+                        b"1 0 R>>startxref\n146\n%%EOF\n"
 
         converter = PdfToJpgConverter()
         image_bytes = converter.convert(pdf_bytes)
