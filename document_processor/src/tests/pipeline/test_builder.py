62cc619f45e94baafd508a191f3f045388c2d3f8
MartinOpat <martinopat.opat@gmail.com>
2023-06-06 17:07:12 +0200

    Refactored and fixed test_builder.py

--- document_processor/src/tests/pipeline/test_builder.py
+++ document_processor/src/tests/pipeline/test_builder.py
@@ -21,23 +21,14 @@ class TestDocumentProcessorPipelineBuilderAbstract:
             DocumentProcessorPipelineBuilder().build(0.5)
 
 
-class TestDocumentProcessorPipelineBuilder:
-    @pytest.fixture(params=[
-        (EffNetDocumentProcessorPipelineBuilder, EffNetDocumentClassifierNode,
-         "./src/document_processor/pipeline/models/effnet"),
-        (EffDetDocumentProcessorPipelineBuilder, EffDetDocumentClassifierNode,
-         "./src/document_processor/pipeline/models/effdet/saved_model/saved_model")
-    ], scope="class")
-    def builder_info(self, request):
-        return request.param
-
+class TestEffNetDocumentProcessorPipelineBuilder:
     @pytest.fixture(scope="class")
-    def builder(self, builder_info):
-        return builder_info[0]()
+    def builder(self):
+        return EffNetDocumentProcessorPipelineBuilder()
 
     @pytest.fixture(scope="class")
     def pipeline(self, builder):
-        return builder.build(0.5)
+        return builder.build(min_confidence=0.5)
 
     def test_initialization(self, builder):
         assert isinstance(builder, DocumentProcessorPipelineBuilder)
@@ -57,10 +48,46 @@ class TestDocumentProcessorPipelineBuilder:
         nodes = pipeline.processing_nodes
         assert isinstance(nodes[0], PdfToImageConverterNode)
 
-    def test_build_second_node_is_correct_type(self, builder_info, pipeline):
+    def test_build_second_node_is_correct_type(self, pipeline):
         nodes = pipeline.processing_nodes
-        assert isinstance(nodes[1], builder_info[1])
+        assert isinstance(nodes[1], EffNetDocumentClassifierNode)
 
-    def test_build_second_node_has_correct_model_path(self, builder_info, pipeline):
+    def test_build_second_node_has_correct_model_path(self, pipeline):
         nodes = pipeline.processing_nodes
-        assert isinstance(nodes[1], builder_info[1])
+        assert isinstance(nodes[1], EffNetDocumentClassifierNode)
+
+
+class TestEffDetDocumentProcessorPipelineBuilder:
+    @pytest.fixture(scope="class")
+    def builder(self):
+        return EffDetDocumentProcessorPipelineBuilder()
+
+    @pytest.fixture(scope="class")
+    def pipeline(self, builder):
+        return builder.build(min_confidence=0.5)
+
+    def test_initialization(self, builder):
+        assert isinstance(builder, DocumentProcessorPipelineBuilder)
+
+    def test_build_returns_pipeline(self, pipeline):
+        assert isinstance(pipeline, DocumentProcessorPipeline)
+
+    def test_build_contains_two_nodes(self, pipeline):
+        nodes = pipeline.processing_nodes
+        assert len(nodes) == 2
+
+    def test_build_first_node_is_pdf_to_image(self, pipeline):
+        nodes = pipeline.processing_nodes
+        assert isinstance(nodes[0], PdfToImageConverterNode)
+
+    def test_build_first_node_has_correct_converter(self, pipeline):
+        nodes = pipeline.processing_nodes
+        assert isinstance(nodes[0], PdfToImageConverterNode)
+
+    def test_build_second_node_is_correct_type(self, pipeline):
+        nodes = pipeline.processing_nodes
+        assert isinstance(nodes[1], EffDetDocumentClassifierNode)
+
+    def test_build_second_node_has_correct_model_path(self, pipeline):
+        nodes = pipeline.processing_nodes
+        assert isinstance(nodes[1], EffDetDocumentClassifierNode)
